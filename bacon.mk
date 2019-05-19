935d995184a5da69ce6b4931c2c579220b965087
Feinzer <danhernan23i@gmail.com>
2019-05-19 17:53:52 +0000

    bacon: Add AOSP apps

--- bacon.mk
+++ bacon.mk
@@ -19,6 +19,11 @@
 DEVICE_PACKAGE_OVERLAYS += \
     $(LOCAL_PATH)/overlay
 
+# AOSP Packages
+PRODUCT_PACKAGES += \
+    messaging \
+    Terminal
+
 # Audio
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/audio/audio_platform_info.xml:$(TARGET_COPY_OUT_VENDOR)/etc/audio_platform_info.xml \
